dec437606eba22b3041b4491801afeecb7b3ab4e
Luke Stevens <lcm.stevens96@gmail.com>
2021-01-26 21:31:55 +0000

    Add Dockerfile and build script

--- src/main/java/uk/co/lukestevens/main/TemplateApiMain.java
+++ src/main/java/uk/co/lukestevens/main/TemplateApiMain.java
@@ -1,14 +1,10 @@
 package uk.co.lukestevens.main;
 
-import java.io.IOException;
-
-import org.apache.commons.cli.ParseException;
-
 import uk.co.lukestevens.app.App;
 
 public class TemplateApiMain {
 
-	public static void main(String[] args) throws ParseException, IOException {
+	public static void main(String[] args) {
 		App.start(new TemplateApiModule());
 	}
 
