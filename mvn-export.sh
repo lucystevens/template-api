7f337b8c41852704b3d231847d5a9a6146486987
Luke Stevens <lcm.stevens96@gmail.com>
2021-01-26 22:25:40 +0000

    Use github set-env for variables

--- mvn-export.sh
+++ mvn-export.sh
@@ -1,4 +1,8 @@
 # Get application properties
-MVN_ARTIFACT=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId|grep -Ev '(^\[|Download\w+:)')
-MVN_GROUP=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.groupId|grep -Ev '(^\[|Download\w+:)')
-MVN_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
+artifact=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId|grep -Ev '(^\[|Download\w+:)')
+group=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.groupId|grep -Ev '(^\[|Download\w+:)')
+version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
+
+echo "::set-env name=MVN_ARTIFACT::$artifact"
+echo "::set-env name=MVN_GROUP::$group"
+echo "::set-env name=MVN_VERSION::$version"
