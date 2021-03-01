62177c8100fbe4d7076eb52967b59dcededecbff
Luke Stevens <lcm.stevens96@gmail.com>
2021-03-01 20:56:29 +0000

    Remove old files

--- scripts/finalise-version.sh
+++
@@ -1,8 +0,0 @@
-# Get version from pom
-version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-
-# Remove suffix if exists
-finalversion=${version%%-*}
-
-# Update version in pom
-mvn versions:set -DnewVersion=$finalversion -q
